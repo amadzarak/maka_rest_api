f0b604144d75195c280e8db1f49dadc0b8b54e08
amadzarak <amadzarak16@gmail.com>
2024-05-01 12:56:52 -0400

    commit

--- maka_connect/maka_connect/asgi.py
+++ maka_connect/maka_connect/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
@@ -18,7 +18,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maka_connect.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-
+from channels.auth import AuthMiddlewareStack
 from maka.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
