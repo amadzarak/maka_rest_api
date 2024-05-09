c2c6d3e410d341737d7152b96fd75efa12fa7b09
amadzarak <amadzarak16@gmail.com>
2024-05-09 16:07:25 -0400

    update

--- maka_connect/apis/views.py
+++ maka_connect/apis/views.py
@@ -261,7 +261,7 @@ def checkMatchStatus(request):
         users.sort()
         current_match_object = Matches.objects.get(user1=users[0], user2=users[1], active=True)
         user_match_serializer = MatchSerializer(current_match_object)
-        print(user_match_serializer)
+        print(user_match_serializer.data)
         return Response(user_match_serializer.data)
     except:
         return Response({"interaction_type": "MatchStatus.none", "current_interaction": True})
