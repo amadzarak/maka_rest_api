5b2abe00a9f21795958b0e515ede0d5c06e7f062
amadzarak <amadzarak16@gmail.com>
2024-05-11 14:05:23 -0400

    update

--- maka_connect/apis/serializers.py
+++ maka_connect/apis/serializers.py
@@ -47,18 +47,24 @@ class MatchSerializer(serializers.ModelSerializer):
     other_user_id = serializers.SerializerMethodField()
 
     def get_other_user_id(self, obj):
-        if obj.user1_id == self.context['user_id']:
-            return obj.user2_id
+        if obj.user1 == self.context['user_id']:
+            return obj.user2
         else:
-            return obj.user1_id
+            return obj.user1
+        #if obj.user1_id == self.context['user_id']:
+        #    return obj.user2_id
+        #else:
+        #    return obj.user1_id
 
     def get_other_user_name(self, obj):
-        if obj.user1_id == self.context['user_id']:
-            profile = Profile.objects.get(user=obj.user2)
-            return profile.nickName
-        else:
-            profile = Profile.objects.get(user=obj.user1)
-            return profile.nickName
+        #if obj.user1_id == self.context['user_id']:
+        #    profile = Profile.objects.get(user=obj.user2)
+        #    return profile.nickName
+        #else:
+        #    profile = Profile.objects.get(user=obj.user1)
+        #    return profile.nickName
+
+        """ Basically the band aid right now is that it will query firebase instead. """
 
     class Meta:
         model = Matches
