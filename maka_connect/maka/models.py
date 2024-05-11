af7b2c2f22da43160a7725f4826a34ec9c87b5a0
amadzarak <amadzarak16@gmail.com>
2024-05-11 13:52:11 -0400

    nullable

--- maka_connect/maka/models.py
+++ maka_connect/maka/models.py
@@ -176,8 +176,8 @@ class UserInteraction(models.Model):
 class Matches(models.Model):
     #user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user1")
     #user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2")
-    user1 = models.TextField()
-    user2 = models.TextField()
+    user1 = models.TextField(null=True)
+    user2 = models.TextField(null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField()
 
