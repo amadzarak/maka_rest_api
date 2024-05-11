8a5a7bfcfe9916d239dc85098870b7a2dc9a04b4
amadzarak <amadzarak16@gmail.com>
2024-05-11 14:39:22 -0400

    update

---
+++ maka_connect/maka/firebase_client.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from django.conf import settings
+
+
+class FirebaseClient:
+
+    def __init__(self):
+        try:
+            firebase_admin.get_app()
+        except ValueError:
+
+
+# Use a service account.
+            cred = credentials.Certificate('~/serviceAccountKey.json')
+            firebase_admin.initialize_app(cred)
+        self._db = firestore.client()
+
+        self._db = firestore.client()
+        self._collection = self._db.collection(u'users')
+
+    def get_by_id(self, id):
+        """Get users on firestore database using document id"""
+        doc_ref = self._collection.document(id)
+        doc = doc_ref.get()
+
+        if doc.exists:
+            return {**doc.to_dict(), "id": doc.id}
+        return
+
+    def all(self):
+        """Get all users from firestore database"""
+        docs = self._collection.stream()
+        return [{**doc.to_dict(), "id": doc.id} for doc in docs]
+
+    def filter(self, field, condition, value):
+        """Filter users using conditions on firestore database"""
+        docs = self._collection.where(field, condition, value).stream()
+        return [{**doc.to_dict(), "id": doc.id} for doc in docs]
