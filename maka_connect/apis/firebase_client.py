899069dd9da6b1d2f13b3f15aa5153b4df7bc732
amadzarak <amadzarak16@gmail.com>
2024-05-11 14:51:31 -0400

    update

--- maka_connect/apis/firebase_client.py
+++ maka_connect/apis/firebase_client.py
@@ -28,9 +28,12 @@ class FirebaseClient:
         doc = doc_ref.get()
 
         if doc.exists:
-            return {**doc.to_dict(), "id": doc.id}
-        return
-
+            print(f"Document data: {doc.to_dict()}")
+            return doc.to_dict()
+        else:
+            print("No such document!")
+        return {}
+        
     def all(self):
         """Get all users from firestore database"""
         docs = self._collection.stream()
