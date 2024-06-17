93d94b8b2e960ed421a334c4c62b32ad66419195
amadzarak <amadzarak16@gmail.com>
2024-06-17 14:47:48 -0400

    update

--- maka_connect/apis/views.py
+++ maka_connect/apis/views.py
@@ -408,7 +408,7 @@ def send_user_alerts(request):
     members = client.get_event_members(request.data['event_id'])
 
     eventActivity = UserInteraction.objects.all().filter(event=request.data['event_id'], current_interaction=True).update(delay=False)
-    
+    """
     tmp = []
     likeAlert = []
     for e in eventActivity:
@@ -483,6 +483,7 @@ def send_user_alerts(request):
 )
 
     matchresponse = messaging.send_multicast(match)
+    """
     return Response({"message": "check terminal"})
 
 
